7b605e9e067b3b8916aa8f2e7ad047a31e9bc2b2
nik-kotine <mswv280407@gmail.com>
2025-08-30 15:02:20 -0500

    fix: minor change

--- seed.js
+++ seed.js
@@ -1,13 +1,12 @@
 const db = require('./database');
 
-// Lista de canciones de ejemplo
 const songs = [
   { title: "Mr. Brightside", artist: "The Killers", year: 2003, album: "Hot Fuss" },
   { title: "Take Me Out", artist: "Franz Ferdinand", year: 2004, album: "Franz Ferdinand" },
   { title: "I Bet You Look Good on the Dancefloor", artist: "Arctic Monkeys", year: 2005, album: "Whatever People Say I Am, That's What I'm Not" },
   { title: "Seven Nation Army", artist: "The White Stripes", year: 2003, album: "Elephant" },
   { title: "Reptilia", artist: "The Strokes", year: 2003, album: "Room on Fire" },
-  { title: "Creep", artist: "Radiohead", year: 1999, album: "Pablo Honey" }
+  { title: "There, There", artist: "Radiohead", year: 2004, album: "Hail to the Thief" }
 ];
 
 songs.forEach(song => {
